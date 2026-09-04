95ebb8034b77592aed49c4238cc7c2fe54cb7785
davidabnersucari <davidabnersucari@gmail.com>
2026-09-04 08:41:07 -0500

    Rama Limpiada

--- DavidPOO/AlgoritmosGUI/src/main/java/pe/edu/upeu/algoritmosgui/HelloApplication.java
+++ DavidPOO/AlgoritmosGUI/src/main/java/pe/edu/upeu/algoritmosgui/HelloApplication.java
@@ -1,45 +1,49 @@
 package pe.edu.upeu.algoritmosgui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.TextField;
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
-        var contenido = "Bienvenido a JavaFX";
-
-        Label mensaje = new Label();
-        mensaje.setStyle("-fx-font-size: 18px; -fx-text-fill: blue;");
-
-        TextField nombre = new TextField();
-        nombre.setPromptText("Escribe tu nombre");
-
+        var contenido="Bienvenido a JavaFX";
+        Label mensaje = new Label(contenido);
+        mensaje.setStyle("-fx-font-size: 18px;");
         Button boton = new Button("Saludar");
+
+
         Button boton1 = new Button("Volver");
+        TextField tf=new TextField();
+
 
         boton.setOnAction(evento -> {
-            String nombreIngresado = nombre.getText();
-            mensaje.setText("¡Hola " + nombreIngresado + "!");
-        });
-        boton1.setOnAction(e -> {
-            mensaje.setText(contenido);
+            String valor=tf.getText();
+            mensaje.setText("Bienvenido "+valor+" !!");
+            mensaje.setStyle("-fx-text-fill: black;");
         });
 
-        VBox raiz = new VBox(15, mensaje, nombre, boton, boton1);
+        boton1.setOnAction(e->{
+
+            mensaje.setText(contenido);
+            mensaje.setStyle("-fx-text-fill: red;");
+
+        });
+
+        VBox raiz = new VBox(15, mensaje, tf,  boton, boton1);
         raiz.setAlignment(Pos.CENTER);
         raiz.setStyle("-fx-padding: 30;");
-
         Scene escena = new Scene(raiz, 400, 250);
-
         stage.setTitle("Hola JavaFX");
         stage.setScene(escena);
         stage.show();
